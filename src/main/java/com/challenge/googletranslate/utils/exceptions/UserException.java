41b0b7e4abeb832e7a80fde2bfa924bdf95c337f
José Santiago Molano Perdomo <santfirax@github.com>
2019-08-20 03:31:43 -0500

    feat(test/Utils): This class represent when the User has Perform an UnsupportedException of a class.
    Signed-off-by: José Santiago Molano Perdomo <santfirax@github.com>

---
+++ src/main/java/com/challenge/googletranslate/utils/exceptions/UserException.java
@@ -0,0 +1,7 @@
+package com.challenge.googletranslate.utils.exceptions;
+
+public class UserException extends UnsupportedOperationException {
+    public UserException(String message) {
+        super(message);
+    }
+}
